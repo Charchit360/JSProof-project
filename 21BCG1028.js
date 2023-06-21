353e5275fcb7d029eb82519878cb48be44f41c92
Charchit360 <131424895+Charchit360@users.noreply.github.com>
2023-06-21 17:32:23 +0530

    Create 21BCG1028.js

---
+++ 21BCG1028.js
@@ -0,0 +1,47 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+let holdnfts = [];
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (par1,par2,par3) {
+    let NFT = {
+        "Name" : par1,
+        "Color": par2,
+        "Price": par3
+    }
+    holdnfts.push(NFT);
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+    for(i = 0;i<holdnfts.length;i++){
+
+        console.log("Name: "+ holdnfts[i].Name);
+        console.log("Color: "+ holdnfts[i].Color);
+        console.log("Price: "+holdnfts[i].Price);
+        console.log();
+    }
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    console.log(holdnfts.length);
+}
+
+// call your functions below this line
+mintNFT("CR7","Green",77);
+mintNFT("Zizou","White",55);
+mintNFT("Beckham","Red",11);
+listNFTs();
+getTotalSupply();
